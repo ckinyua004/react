59f06034fe49f22c8f0e72a99482b4d63e7ef4dc
ckinyua004 <njauc85@gmail.com>
2025-07-11 22:31:05 +0300

    Add full project files

---
+++ 02-Vanilla-js/script.js
@@ -0,0 +1,94 @@
+const bookData = [
+  {
+    id: 1,
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature"
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: "El señor de los anillos",
+      chinese: "魔戒",
+      french: "Le Seigneur des anneaux"
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 4766
+      }
+    }
+  },
+  {
+    id: 2,
+    title: "Dune",
+    publicationDate: "1965-08-01",
+    author: "Frank Herbert",
+    genres: [
+      "science fiction",
+      "adventure",
+      "epic",
+      "novels"
+    ],
+    hasMovieAdaptation: true,
+    pages: 412,
+    translations: {
+      spanish: "Dune",
+      chinese: "沙丘",
+      french: "Dune"
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.24,
+        ratingsCount: 1700000,
+        reviewsCount: 20000
+      },
+      librarything: {
+        rating: 4.35,
+        ratingsCount: 5000
+      }
+    }
+  },
+  {
+    id: 3,
+    title: "1984",
+    publicationDate: "1949-06-08",
+    author: "George Orwell",
+    genres: [
+      "dystopian",
+      "fiction",
+      "novels",
+      "literature"
+    ],
+    hasMovieAdaptation: true,
+    pages: 328,
+    translations: {
+      spanish: "1984",
+      chinese: "一九八四",
+      french: "1984"
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.17,
+        ratingsCount: 800000,
+        reviewsCount: 10000
+      },
+      librarything: {
+        rating: 4.25,
+        ratingsCount: 12000
+      }
+    }
+  }
+];
